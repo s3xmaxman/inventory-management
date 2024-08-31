586521e58c4aba5d4ee23e7f731f544fa8fddafb
s3xmaxman <badside3919@gmail.com>
2024-08-31 10:03:11 +0900

    update layout.tsx, page.tsx and tailwind.config.ts

---
+++ client/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default DashboardWrapper;
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,70 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+/**
+ * テーマオブジェクトを生成する関数
+ *
+ * @param colors - 色のオブジェクト (any)
+ * @param mapping - シェードマッピングのオブジェクト (any)
+ * @param invert - シェードを反転させるかどうかのフラグ (boolean, デフォルトは false)
+ * @returns 生成されたテーマオブジェクト (any)
+ *
+ * この関数は、指定された色とマッピングに基づいてテーマオブジェクトを生成します。
+ * invert が true の場合、シェードマッピングが反転されます。
+ *
+ * @throws エラーは発生しませんが、無効な色やマッピングが渡された場合は未定義の動作を引き起こす可能性があります。
+ */
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+};
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkTheme = generateThemeObject(colors, shadeMapping, true);
+
+const themes = {
+  light: {
+    ...lightTheme,
+    white: "#ffffff",
+  },
+  dark: {
+    ...darkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+  },
+};
 
 const config: Config = {
   content: [
@@ -15,6 +81,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 };
 export default config;
