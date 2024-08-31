cee5d71b20ac1719815a8e9790f6d1eb21629f67
s3xmaxman <badside3919@gmail.com>
2024-08-31 10:16:59 +0900

    update dashboardWrapper.tsx

--- client/app/dashboardWrapper.tsx
+++ client/app/dashboardWrapper.tsx
@@ -3,7 +3,17 @@ import React from "react";
 type Props = {};
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-grey-50 md:pl-24`}
+      >
+        Navbar
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default DashboardWrapper;
