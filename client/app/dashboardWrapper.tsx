995eadc15dbf4ae62b8c210fee6b7d45d8559535
s3xmaxman <badside3919@gmail.com>
2024-09-01 09:05:57 +0900

    update dashboardWrapper.tsx, Navbar.tsx and Sidebar.tsx

--- client/app/dashboardWrapper.tsx
+++ client/app/dashboardWrapper.tsx
@@ -1,15 +1,34 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-import React from "react";
-import StoreProvider from "./redux";
+import React, { useEffect } from "react";
+import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
   return (
-    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <Sidebar />
       <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-grey-50 md:pl-24`}
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,17 +1,33 @@
 "use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 import { Bell, Menu, Settings, Sun } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* Left side */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
@@ -31,7 +47,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
+            <button onClick={toggleDarkMode}>
               <Sun className="cursor-pointer text-gray-500" size={24} />
             </button>
           </div>
